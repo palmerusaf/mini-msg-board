a6a5a4b3a61d7f84e27707525ac2dec05f3f1b1d
Branden Palmer <brandenpalmer08@gmail.com>
2024-02-02 16:04:56 +0700

    add form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini Messageboard' });
+});
+
+router.post('/new', (req, res) => {
+  // console.log({ added: Date.now(), user: req.body.userName, text: req.body.messageText })
+  messages.push({ added: new Date(), user: req.body.userName, text: req.body.messageText });
+  res.redirect('/');
+})
+
 module.exports = router;

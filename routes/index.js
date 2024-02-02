c95522bf8c754251524ac51e089456a443506a50
Branden Palmer <brandenpalmer08@gmail.com>
2024-02-02 16:30:53 +0700

    update templates

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
 ];
 
 /* GET home page. */
